319f860486dc3439e1e25259b9b12f5429f46e16
KornyIsDeveloping <132944959+KornyIsDeveloping@users.noreply.github.com>
2024-04-16 13:55:48 -0700

    create update delete done

--- play_ground/resources/views/products/create.blade.php
+++ play_ground/resources/views/products/create.blade.php
@@ -40,7 +40,7 @@
                         <label for="price" class="block text-sm font-medium leading-6 text-gray-900">Price</label>
                         <div class="mt-2">
                             <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="price" id="price" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="30.99 EUR" required>
+                                <input type="text" name="price" id="price" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="30.99" required>
                             </div>
 
                             @error('price')
--- play_ground/resources/views/products/edit.blade.php
+++ play_ground/resources/views/products/edit.blade.php
@@ -9,16 +9,53 @@
             <div class="border-b border-gray-900/10 pb-12">
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Name of the Game</label>
+                        <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Name of the Game</label>
                         <div class="mt-2">
                             <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                 <input
                                     type="text"
-                                    name="title"
-                                    id="title"
+                                    name="name"
+                                    id="name"
                                     value="{{ $product->name }}"
                                     class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="PHP Developer"
+                                    placeholder="Mortal Kombat 11"
+                                    required>
+                            </div>
+
+                            @error('name')
+                            <p class="text-xs text-red-400 font-semibold mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="sm:col-span-4">
+                        <label for="description" class="block text-sm font-medium leading-6 text-gray-900">Description</label>
+                        <div class="mt-2">
+                            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                <textarea
+                                    type="text"
+                                    name="description"
+                                    id="description"
+                                    value="{{ $product->description }}"
+                                    class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    placeholder="Edit your listing game information..."
+                                    required></textarea>
+                            </div>
+                            @error('description')
+                            <p class="text-xs text-red-400 font-semibold mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="sm:col-span-4">
+                        <label for="price" class="block text-sm font-medium leading-6 text-gray-900">Price</label>
+                        <div class="mt-2">
+                            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                <input
+                                    type="text"
+                                    name="price"
+                                    id="price"
+                                    value="{{ $product->price }}"
+                                    class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    placeholder="30.99"
                                     required>
                             </div>
 
@@ -28,20 +65,39 @@
                         </div>
                     </div>
                     <div class="sm:col-span-4">
-                        <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Price</label>
+                        <label for="currency" class="block text-sm font-medium leading-6 text-gray-900">Currency</label>
                         <div class="mt-2">
                             <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                 <input
                                     type="text"
-                                    name="salary"
-                                    id="salary"
-                                    value="{{ $product->price }}"
+                                    name="currency"
+                                    id="currency"
+                                    value="{{ $product->currency }}"
                                     class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="$50.000 Per Year"
+                                    placeholder="EUR/USD/RON"
                                     required>
                             </div>
 
-                            @error('salary')
+                            @error('currency')
+                            <p class="text-xs text-red-400 font-semibold mt-1">{{ $message }}</p>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="sm:col-span-4">
+                        <label for="stock" class="block text-sm font-medium leading-6 text-gray-900">Stock</label>
+                        <div class="mt-2">
+                            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                <input
+                                    type="text"
+                                    name="stock"
+                                    id="stock"
+                                    value="{{ $product->stock }}"
+                                    class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    placeholder="10"
+                                    required>
+                            </div>
+
+                            @error('title')
                             <p class="text-xs text-red-400 font-semibold mt-1">{{ $message }}</p>
                             @enderror
                         </div>
@@ -55,9 +111,9 @@
                 <button form="delete-form" type="submit" class="text-red-400 text-sm font-bold">Delete</button>
             </div>
             <div class="flex items-center gap-x-6">
-                <a href="/products/{{ $product->id }}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+                <a href="/products" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
                 <div>
-                    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+                    <button href="/products/" type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
                 </div>
             </div>
         </div>

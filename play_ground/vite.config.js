fe2507114ff7d0920b9eb20aeecc8bb714e65c38
KornyIsDeveloping <132944959+KornyIsDeveloping@users.noreply.github.com>
2024-04-29 13:46:59 -0700

    dark and light mode implemented

--- play_ground/vite.config.js
+++ play_ground/vite.config.js
@@ -1,11 +1,33 @@
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
     ],
+    define: {
+        'global': 'window'
+    },
+    optimizeDeps: {
+        include: ["jquery"]
+    },
+    build: {
+        rollupOptions: {
+            plugins: [
+                {
+                    name: 'load-jquery-correctly',
+                    load(id) {
+                        if (id.endsWith('node_modules/jquery/dist/jquery.js')) {
+                            return 'window.jQuery = window.$ = require("jquery");';
+                        }
+                    }
+                }
+            ]
+        }
+    }
 });

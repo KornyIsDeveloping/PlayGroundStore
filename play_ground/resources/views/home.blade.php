857143ed78b0319fd36df89231a3f89621643a71
KornyIsDeveloping <132944959+KornyIsDeveloping@users.noreply.github.com>
2024-04-29 15:58:33 -0700

    minor changes to the home screen

--- play_ground/resources/views/home.blade.php
+++ play_ground/resources/views/home.blade.php
@@ -1,19 +1,68 @@
 <x-layout>
     <x-slot:heading>
-        Home Page
     </x-slot:heading>
-    <div>
-        <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
-            <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+{{--    <div>--}}
+{{--        <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">--}}
+{{--            <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>--}}
+{{--        </div>--}}
+{{--        <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">--}}
+{{--            <div class="text-center">--}}
+{{--                <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Welcome to Play Ground Store</h1>--}}
+{{--                <p class="mt-6 text-lg leading-8 text-gray-600">Brings you closer to <span>THE GAME</span></p>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">--}}
+{{--            <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+
+    <div class="relative max-w-xl mx-auto mt-20">
+        <img class="h-64 w-full object-cover rounded-md" src="https://images.unsplash.com/photo-1511512578047-dfb367046420?q=80&w=2342&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Retro gaming room">
+        <div class="absolute inset-0 bg-gray-700 opacity-60 rounded-md"></div>
+        <div class="absolute inset-0 flex items-center justify-center">
+            <h2 class="text-white text-3xl font-bold">Play Ground Store</h2>
         </div>
-        <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-            <div class="text-center">
-                <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Welcome to Play ground Store</h1>
-                <p class="mt-6 text-lg leading-8 text-gray-600">Brings you closer to <span>THE GAME</span></p>
+    </div>
+    <div class="w-full flex justify-center items-center">
+
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:py-24 lg:px-8">
+            <div class="grid grid-cols-1 gap-5 sm:grid-cols-4 mt-4">
+                <div class="bg-white overflow-hidden shadow sm:rounded-lg dark:bg-gray-900">
+                    <div class="px-4 py-5 sm:p-6">
+                        <dl>
+                            <dt class="text-sm leading-5 font-medium text-gray-500 truncate dark:text-gray-400">Total users</dt>
+                            <dd class="mt-1 text-3xl leading-9 font-semibold text-indigo-600 dark:text-indigo-400">1.6M</dd>
+                        </dl>
+                    </div>
+                </div>
+                <div class="bg-white overflow-hidden shadow sm:rounded-lg dark:bg-gray-900">
+                    <div class="px-4 py-5 sm:p-6">
+                        <dl>
+                            <dt class="text-sm leading-5 font-medium text-gray-500 truncate dark:text-gray-400">Our products</dt>
+                            <dd class="mt-1 text-3xl leading-9 font-semibold text-indigo-600 dark:text-indigo-400">19.2K
+                            </dd>
+                        </dl>
+                    </div>
+                </div>
+                <div class="bg-white overflow-hidden shadow sm:rounded-lg dark:bg-gray-900">
+                    <div class="px-4 py-5 sm:p-6">
+                        <dl>
+                            <dt class="text-sm leading-5 font-medium text-gray-500 truncate dark:text-gray-400">Last added products</dt>
+                            <dd class="mt-1 text-3xl leading-9 font-semibold text-indigo-600 dark:text-indigo-400">4.9K</dd>
+                        </dl>
+                    </div>
+                </div>
+                <div class="bg-white overflow-hidden shadow sm:rounded-lg dark:bg-gray-900">
+                    <div class="px-4 py-5 sm:p-6">
+                        <dl>
+                            <dt class="text-sm leading-5 font-medium text-gray-500 truncate dark:text-gray-400">Comments
+                            </dt>
+                            <dd class="mt-1 text-3xl leading-9 font-semibold text-indigo-600 dark:text-indigo-400">166.7K
+                            </dd>
+                        </dl>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
-            <div class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
         </div>
     </div>
 </x-layout>

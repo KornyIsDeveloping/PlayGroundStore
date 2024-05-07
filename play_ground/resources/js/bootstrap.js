6de457430c24d21be4799fc3bc44b8054ffff614
KornyIsDeveloping <132944959+KornyIsDeveloping@users.noreply.github.com>
2024-05-07 12:04:27 -0700

    working on fetching the datas into the home page

--- play_ground/resources/js/bootstrap.js
+++ play_ground/resources/js/bootstrap.js
@@ -11,11 +11,6 @@ window.Pusher = Pusher;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-// import $ from 'jquery'
-// window.jQuery = window.$ = $
-//
-//
-// window.Pusher = require('pusher-js');
 
 import Echo from 'laravel-echo';
 
@@ -23,17 +18,9 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    encrypted: true,
     forceTLS: true
 });
-
-// window.Pusher = require('pusher-js');
-//
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key'
-// });
-
-
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -41,18 +28,3 @@ window.Echo = new Echo({
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo';
-
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-//     wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-//     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-//     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-// });

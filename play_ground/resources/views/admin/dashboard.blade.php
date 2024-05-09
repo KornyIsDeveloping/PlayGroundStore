5b5a0596c0844513c3477f8a8396691811cbd560
KornyIsDeveloping <132944959+KornyIsDeveloping@users.noreply.github.com>
2024-05-09 00:36:32 -0700

    dashboard done

--- play_ground/resources/views/admin/dashboard.blade.php
+++ play_ground/resources/views/admin/dashboard.blade.php
@@ -4,13 +4,14 @@
     </x-slot:heading>
 
     <div class="container mx-auto px-4 py-8">
-        <h1 class="text-3xl font-bold mb-4">Admin Dashboard</h1>
+        <h1 class="dark:text-custom-dark-text text-3xl font-bold mb-4">Admin Dashboard</h1>
         <div class="overflow-x-auto">
             <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th scope="col" class="py-3 px-6">User Email</th>
                     <th scope="col" class="py-3 px-6">Comments</th>
+                    <th scope="col" class="py-3 px-6">Admin</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -19,9 +20,10 @@
                         <td class="py-4 px-6">{{ $user->email }}</td>
                         <td class="py-4 px-6">
                             @foreach ($user->comments as $comment)
-                                <div>{{ $comment->content }}</div> <!-- Replace 'content' with your actual comment field -->
+                                <div>{{ $comment->body }}</div>
                             @endforeach
                         </td>
+                        <td>{{ $user->is_admin ? 'Yes' : 'No' }}</td>
                     </tr>
                 @endforeach
                 </tbody>

0ae8c711e51bed74a5fe54458cbcf3f25bb47203
KornyIsDeveloping <132944959+KornyIsDeveloping@users.noreply.github.com>
2024-05-09 04:46:38 -0700

    design changes

--- play_ground/resources/views/admin/users/create.blade.php
+++ play_ground/resources/views/admin/users/create.blade.php
@@ -3,24 +3,24 @@
     </x-slot:heading>
 
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <h2 class="text-2xl font-bold mb-6">Create New User</h2>
+            <h2 class="text-custom-dark-text text-2xl font-bold mb-6">Create New User</h2>
             <div class="dark:bg-custom-dark overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 dark:bg-custom-dark border-b border-gray-200">
                     <form method="POST" action="{{ route('admin.users.store') }}">
                         @csrf
                         <div class="mb-4">
                             <label for="name" class="block text-sm font-medium text-custom-dark-text">Name</label>
-                            <input type="text" name="name" id="name" required class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                            <input type="text" name="name" id="name" required class="mt-1 block w-full dark:bg-gray-800 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                         </div>
 
                         <div class="mb-4">
                             <label for="email" class="block text-sm font-medium text-custom-dark-text">Email</label>
-                            <input type="email" name="email" id="email" required class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                            <input type="email" name="email" id="email" required class="mt-1 block w-full dark:bg-gray-800 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                         </div>
 
                         <div class="mb-4">
                             <label for="password" class="block text-sm font-medium text-custom-dark-text">Password</label>
-                            <input type="password" name="password" id="password" required class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                            <input type="password" name="password" id="password" required class="mt-1 block w-full dark:bg-gray-800 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                         </div>
 
                         <div class="flex items-center justify-end mt-4">
